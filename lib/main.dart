219037d8ad48450da3ceea7094e9c3185ebe0eac
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-08-22 13:33:05 -0300

    Classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:codefriend/views/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: null,
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primarySwatch: Colors.deepOrange
+    ),
+    home: HomePage(),
   ));
 }
