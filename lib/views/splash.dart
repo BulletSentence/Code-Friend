075f58a31728cab5cac57191780fcd230793e0a6
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-08-23 13:15:20 -0300

    Flare for splash

---
+++ lib/views/splash.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          child: FlareActor('assets/PulltoRefresh.flr', animation: 'Opacity'),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3)).then((_) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => Home()),
+      );
+    });
+  }
+}
