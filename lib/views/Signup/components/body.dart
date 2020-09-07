5608e33c9e6dff5e38fad2bffd03307a4bf6398b
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-09-07 16:25:43 -0300

    add scoped

--- lib/views/Signup/components/body.dart
+++ lib/views/Signup/components/body.dart
@@ -2,11 +2,13 @@ import 'package:codefriend/components/already_have_an_account_acheck.dart';
 import 'package:codefriend/components/rounded_button.dart';
 import 'package:codefriend/components/rounded_input_field.dart';
 import 'package:codefriend/components/rounded_password_field.dart';
+import 'package:codefriend/models/user_model.dart';
 import 'package:codefriend/views/Login/components/background.dart';
 import 'package:codefriend/views/Login/login_screen.dart';
 import 'package:codefriend/views/Signup/components/social_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:scoped_model/scoped_model.dart';
 import 'or_divider.dart';
 
 class Body extends StatelessWidget {
@@ -15,55 +17,63 @@ class Body extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Background(
       child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(height: size.height * 0.04),
-            SvgPicture.asset(
-              "assets/icons/create.svg",
-              height: size.height * 0.35,
-            ),
-            RoundedInputField(
-              hintText: "Email",
-              onChanged: (value) {},
-            ),
-            RoundedPasswordField(
-              onChanged: (value) {},
-            ),
-            RoundedButton(
-              text: "CRIAR CONTA",
-              press: () {},
-            ),
-            SizedBox(height: size.height * 0.03),
-            AlreadyHaveAnAccountCheck(
-              login: false,
-              press: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return LoginScreen();
-                    },
+        child:ScopedModelDescendant<UserModel>(builder: (context, child, model) {
+          if (model.isLoading)
+            return Center(
+              child: CircularProgressIndicator(),
+              heightFactor: 10,
+              widthFactor: 10,
+            );
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: size.height * 0.04),
+              SvgPicture.asset(
+                "assets/icons/create.svg",
+                height: size.height * 0.35,
+              ),
+              RoundedInputField(
+                hintText: "Email",
+                onChanged: (value) {},
+              ),
+              RoundedPasswordField(
+                onChanged: (value) {},
+              ),
+              RoundedButton(
+                text: "CRIAR CONTA",
+                press: () {},
+              ),
+              SizedBox(height: size.height * 0.03),
+              AlreadyHaveAnAccountCheck(
+                login: false,
+                press: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return LoginScreen();
+                      },
+                    ),
+                  );
+                },
+              ),
+              OrDivider(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SocalIcon(
+                    iconSrc: "assets/icons/facebook.svg",
+                    press: () {},
                   ),
-                );
-              },
-            ),
-            OrDivider(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SocalIcon(
-                  iconSrc: "assets/icons/facebook.svg",
-                  press: () {},
-                ),
-                SocalIcon(
-                  iconSrc: "assets/icons/google-plus.svg",
-                  press: () {},
-                ),
-              ],
-            )
-          ],
-        ),
+                  SocalIcon(
+                    iconSrc: "assets/icons/google-plus.svg",
+                    press: () {},
+                  ),
+                ],
+              )
+            ],
+          );
+        }),
       ),
     );
   }
